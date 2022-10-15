61136bd1ed7b84a8583f682d89876e43d3f13b88
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-14 21:56:52 -0300

    Create teste003.py

---
+++ Curso Python/pythonExercicios/teste003.py
@@ -0,0 +1,21 @@
+soma = 5 + 2 == 7
+print(soma)
+soma = 5-2 == 3
+print(soma)
+soma = 5*2 == 10
+print(soma)
+soma = 5/2 == 2.5
+print(soma)
+soma = 5**2 == 25
+print(soma)
+soma = 5 // 2 == 2
+print(soma)
+soma = 5 % 2 == 1
+print(soma)
+soma = 5+3*2
+print('{}'.format(soma))
+soma = 3*5+4**2
+print('{}'.format(soma))
+soma = 3*(5+4)**2
+print('{}'.format(soma),type(soma))
+
