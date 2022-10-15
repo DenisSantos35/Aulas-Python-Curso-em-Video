a305e983426f18dfc62519b00678650f783cc673
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-14 21:41:14 -0300

    Create aula08a.py

---
+++ Curso Python/pythonExercicios/aula08a.py
@@ -0,0 +1,19 @@
+#import math
+#from math import sqrt, floor, factorial
+#import random
+#n = random.random()
+#s = random.randint(1,100)
+#print(s)
+#print(n)
+#num = int(input('Digite um numero: '))
+#raiz = sqrt(num)
+#print('A raiz de {} e igual a {:.0f}'.format(num, floor(raiz)))
+#n = int(input('digite n: '))
+#b = int(input('digite b: '))
+#n = factorial(n)
+#print(n)
+#b = factorial(b)
+#print(b)
+#import emoji
+#print(emoji.emojize("ola mundo :sunglasses:", use_aliases=True))
+
