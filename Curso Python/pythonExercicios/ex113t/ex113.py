b12aeb382ed7dcee76e5bb9ff22d9110cd32f268
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-14 21:55:04 -0300

    Create ex113.py

---
+++ Curso Python/pythonExercicios/ex113t/ex113.py
@@ -0,0 +1,4 @@
+from tratamento import leiaint, leiareal
+inteiro = leiaint('Digite um numero')
+real = leiareal('Digite um Real')
+print(f'O valor inteiro digitado foi {inteiro} e o real foi {real} ')
