e7ba34e3160036c47429295d3752be62810569a7
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-14 21:39:27 -0300

    Create aula013repetiçao.py

---
+++ Curso Python/pythonExercicios/aula013repetiçao.py
@@ -0,0 +1,23 @@
+'''for i in range(0, 6, 2):
+    print('{}: oi'.format(i + 1))
+print('fim')
+for i in range(6, 0, -2):
+    print(i)
+print('fim')'''
+
+'''n = int(input('Digite um numero: '))
+for c in range(0,n + 1):
+    print(c)'''
+'''ini = int(input())
+fim= int(input())
+pas = int(input())
+
+for c in range(ini, fim, pas):
+    print(c)'''
+s = 0
+for c in range(0,3):
+    n = int(input('Digite um valor'))
+    s += n
+print('Fim', s)
+
+
