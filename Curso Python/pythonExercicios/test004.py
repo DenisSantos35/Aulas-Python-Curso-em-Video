d132d9583eaaabc1ba542416d92dcdc996dd6f5e
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-14 21:56:33 -0300

    Create test004.py

---
+++ Curso Python/pythonExercicios/test004.py
@@ -0,0 +1,10 @@
+n1 = int(input())
+n2 = int(input())
+print('A soma vale {}'.format(n1+n2))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('A soma e {}, \nO produto e {}, \nA divisao e {:.2f}'.format(s, m, d), end='>>>')
+print('A divisao inteira {}, \nA potencia {}'.format(di, e))
