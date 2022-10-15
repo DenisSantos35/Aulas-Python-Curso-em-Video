acfb5b2fada0bb167e4d06be1ab5d002c59dd1db
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-14 21:38:42 -0300

    Create aula010.py

---
+++ Curso Python/pythonExercicios/aula010.py
@@ -0,0 +1,9 @@
+tempo = int(input('How old have your car?'))
+if tempo <= 3:
+    print('your is New car')
+else:
+    print('Your is Old car')
+print('-------End--------')
+
+print('Carro Novo'if tempo<=3 else 'Carro Velho')
+
