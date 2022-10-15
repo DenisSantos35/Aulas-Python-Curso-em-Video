e7c32009ece4228f3a552c00cf9467b1933890ea
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-14 21:54:11 -0300

    Create ex097.py

---
+++ Curso Python/pythonExercicios/ex097.py
@@ -0,0 +1,12 @@
+def escreva(msg):
+    print('~'*len(msg))
+    print(msg)
+    print('~'*len(msg))
+
+
+
+escreva('  GUSTAVO GUANABARA  ')
+escreva('  CURSO DE PYTHON NO YOU TUBE  ')
+escreva('  CURSO DE PYTHON NO CURSO EM VIDEO  ')
+escreva('   Denis Diogo dos Santos  ')
+
