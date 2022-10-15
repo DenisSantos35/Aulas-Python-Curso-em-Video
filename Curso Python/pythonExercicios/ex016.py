e6b2c23ce44078e172fa63ebc12feb6c7b852452
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-14 21:44:13 -0300

    Create ex016.py

---
+++ Curso Python/pythonExercicios/ex016.py
@@ -0,0 +1,6 @@
+'''from math import trunc
+numero = float(input('Digite um numero: '))
+print('A parte inteira do numero {} e igual a {}!!'.format(numero,trunc(numero)))'''
+#ou
+numero = float(input('Digite um valor: '))
+print('A parte inteira do numero{} e igual a {}!!!'.format(numero, int(numero)))
