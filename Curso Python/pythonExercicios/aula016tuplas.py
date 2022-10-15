2b6a828d1aca2cce097b138f7caaefa90fd0cc93
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-14 21:40:21 -0300

    Create aula016tuplas.py

---
+++ Curso Python/pythonExercicios/aula016tuplas.py
@@ -0,0 +1,44 @@
+    lanche = ('hamburguer', 'pizza', 'suco', 'pudim') #tupla pode escrever com parenteses
+lanche = 'hamburguer', 'pizza', 'suco', 'pudim' #ou sem parenteses
+#tuplas sao imutaveis
+# nao se consegue atribuir valores a tupla lanche[1] = 'refrigerante' dara erro
+'''print(lanche)
+print(lanche[0])
+print(lanche[1])
+print(lanche[2])
+print(lanche[3])
+print(lanche[-2])
+print(lanche[-4])
+print(lanche[1:3])
+print(lanche[2:])
+print(lanche[:2])
+print(lanche[-2:])
+print(lanche[:-1])
+print(len(lanche))
+#for cont in range(0, len(lanche)):
+    #print(lanche[cont])
+#for comida in lanche:
+    #print(f'Eu vou comer {comida}\n')
+#for pos, comida in enumerate(lanche):
+    #print(f'Eu vou comer {comida} e na posiçao{pos}')
+print(sorted(lanche))
+print(lanche)'''
+'''a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = (a) + (b)
+d = b + a
+print('d=',d)
+print('c =', c)
+print(a)
+print(type(a))
+print(len(c))
+print(c.count(5)) #quantos 5 tem na tupla
+print(c.index(2)) #qual posiçao do elemento 2
+print(c.index(2,3)) # apartir da posiçao 3 qual a posiçao do elmento 2'''
+pessoa = ('Denis', 31, 'M', 50.00)
+
+print(pessoa)
+print(sorted(lanche))
+
+
+
